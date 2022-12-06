fdab88de35e275d99a1526726b1d09711cb83d4e
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-12-06 22:17:26 +0530

    started binary search

--- src/Searching_Algorithms/Binary_search.java
+++ src/Searching_Algorithms/Binary_search.java
@@ -1,4 +1,18 @@
 package Searching_Algorithms;
+import java.util.*;
+import java.lang.*;
 
 public class Binary_search {
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter number of items : ");
+        int n = in.nextInt();
+        ArrayList<Integer> list = new ArrayList<Integer>(n);
+        int item;
+        for(int i=0;i<n;i++){
+            item = in.nextInt();
+            list.add(item);
+        }
+        System.out.println(list);
+    }
 }
