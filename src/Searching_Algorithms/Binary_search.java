111baeaa8d803cf90962fad6345a1ab873bfe5ed
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-12-07 09:08:56 +0530

    adding searching algorithms

--- src/Searching_Algorithms/Binary_search.java
+++ src/Searching_Algorithms/Binary_search.java
@@ -5,14 +5,44 @@ import java.lang.*;
 public class Binary_search {
     public static void main(String[] arg){
         Scanner in = new Scanner(System.in);
+        int s_item = 1; //Search Element
+
         System.out.println("Enter number of items : ");
         int n = in.nextInt();
+
+        //Creating an Arraylist
         ArrayList<Integer> list = new ArrayList<Integer>(n);
         int item;
+
+        //Adding items to list in sorted manner
         for(int i=0;i<n;i++){
             item = in.nextInt();
             list.add(item);
         }
-        System.out.println(list);
+
+        //initializing the mid-index
+        int ind = list.size()/2;
+        boolean found = false;
+
+        while(ind>=0 && ind<list.size()){
+            if(list.get(ind) == s_item){
+                System.out.println("Item is on index : "+ind);
+                found = true;
+                break;
+
+            }
+            else if(list.get(ind)>s_item){
+                ind--;
+            }
+
+            else if(list.get(ind)<s_item){
+                ind++;
+            }
+        }
+
+        if(!found){
+            System.out.println("Item not found");
+        }
+
     }
 }
--- src/Searching_Algorithms/Linear_search.java
+++ src/Searching_Algorithms/Linear_search.java
@@ -1,4 +1,41 @@
 package Searching_Algorithms;
+import java.util.*;
+import java.lang.*;
 
 public class Linear_search {
+    public static void main(String[] arg){
+        Scanner in = new Scanner(System.in);
+
+        int s_item = 2;
+
+        System.out.print("Enter size of the list : ");
+        int size = in.nextInt();
+        System.out.println();
+
+        //Creating an ArrayList
+        ArrayList<Integer> list = new ArrayList<Integer>(size);
+
+        //Adding Items to it in any manner
+        for(int i=0;i<size;i++){
+            System.out.print("Enter element no. "+i+" : ");
+            list.add(in.nextInt());
+        }
+
+        boolean found = false;
+
+
+        //Linearly traversing through list to search the element
+        for(int i=0;i<list.size();i++){
+            if(list.get(i) == s_item){
+                System.out.println("The item in on index : "+i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found){
+            System.out.println("Item not found");
+        }
+
+    }
 }
