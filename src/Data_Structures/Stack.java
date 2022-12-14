8d1707f62d0f66ca9befc693c62eca15fab91fcc
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-12-14 22:28:00 +0530

    Adding Stack

--- src/Data_Structures/Stack.java
+++ src/Data_Structures/Stack.java
@@ -1,4 +1,70 @@
 package Data_Structures;
+import java.util.*;
+
+
+class StackOperation{
+    int size,index;
+    int[] arr;
+    StackOperation(int size){
+        this.size = size;
+        arr = new int[size];
+        index = -1;
+    }
+
+    public void push(int item){
+        if(index==size-1){
+            System.out.println("Stack Overflow");
+        }
+        else{
+            index++;
+            arr[index] = item;
+        }
+
+    }
+
+    public void pop(){
+        if(index<0){
+            System.out.println("Stack Underflow");
+        }
+
+        else{
+            System.out.println("Popped item: "+ arr[index--]);
+        }
+    }
+
+    public void printStack(){
+        for(int i=index;i>=0;i--){
+            if(i == 0){
+                System.out.print(arr[i]);
+            }
+            else{
+                System.out.print(arr[i]+"->");
+            }
+        }
+        System.out.println();
+    }
+}
 
 public class Stack {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter size of Stack : ");
+        int size = in.nextInt();
+
+        StackOperation obj = new StackOperation(size);
+        obj.push(2);
+        obj.push(3);
+        obj.push(4);
+        obj.push(5);
+        obj.push(6);
+        obj.printStack();
+        obj.pop();
+        obj.pop();
+        obj.printStack();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.printStack();
+    }
 }
