dae70d53c76e363b389f7caad72fb36f144e0401
Aashutosh Sarode <a.onelife0033@gmail.com>
2022-12-20 18:02:51 +0530

    adding modified merge sort

--- src/Sorting_Algorithms/Merge_sort.java
+++ src/Sorting_Algorithms/Merge_sort.java
@@ -3,20 +3,64 @@ import java.util.*;
 
 public class Merge_sort{
 
-    static int[] sort(int[] arr,int low, int high){
+    static void merge(int[] arr, int low, int mid, int high){
+        int n1 = mid - low + 1;
+        int n2 = high - mid;
 
-        if(low> high){
+        int[] L = new int[n1];
+        int[] R = new int[n2];
+
+        for(int i=0;i<n1;i++){
+            L[i] = arr[low+i];
+        }
+        for(int j=0;j<n2;j++){
+            R[j] = arr[mid+1+j];
+        }
+        int i =0, j=0;
+        int k = low;
+
+        while(i<n1 && j<n2){
+            if(L[i] <= R[j]){
+                arr[k] = L[i];
+                i++;
+            }
+            else{
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < n1){
+            arr[k] =   L[i];
+            i++;
+            k++;
+        }
+
+        while(j<n2){
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+
+        for(int p : arr){
+            System.out.println(p);
+        }
+
+    }
+
+    static void sort(int[] arr,int low, int high){
+
+        if(low < high){
             int mid = low+(high - low)/2;
             sort(arr, low, mid);
             sort(arr,mid+1,high);
 
-            merge(arr,)
+            merge(arr,low,mid,high);
+
 
         }
 
-
-
-        return arr;
     }
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
